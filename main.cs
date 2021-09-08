3707c4ea373d11065d19d01506b864d7e3da116e
Amy Jones <70500009+amyjprogrammer@users.noreply.github.com>
2021-09-08 19:08:08 +0000

    Added switch

--- main.cs
+++ main.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
+    /*
     for (int i = 1; i <= 100; i++){
       if (i % 3 == 0 && i % 5 == 0){
         Console.WriteLine("FizzBuzz");
@@ -12,6 +13,36 @@ class Program {
       } else {
         Console.WriteLine(i);
       }
+      
+    }
+    */
+
+    Console.Write("Please select a number between 1-6: ");
+    int numb1 = Convert.ToInt32(Console.ReadLine());
+    
+
+    switch(numb1){
+      case 1: 
+      Console.WriteLine("Archimedes");
+      break;
+      case 2:
+      Console.WriteLine("Thor");
+      break;
+      case 3:
+      Console.WriteLine("Ripley");
+      break;
+      case 4:
+      Console.WriteLine("Tristan");
+      break;
+      case 5:
+      Console.WriteLine("Jalapeno");
+      break;
+      case 6:
+      Console.WriteLine("Athena");
+      break;
+      default:
+      Console.WriteLine("I miss you Pippin, Callie and Maverick!");
+      break;
     }
   }
 }
